628b039d43c76f25554376aa2fb0fdee8c5831b9
willem267 <truong260703@gmail.com>
2024-12-15 17:20:21 +0700

    chỉnh sửa st

--- Mysql/db_config.php
+++ Mysql/db_config.php
@@ -10,8 +10,8 @@ try {
   $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Kết nối thành công";
+ 
 } catch(PDOException $e) {
-  echo "Kết nối thất bại " . $e->getMessage();
+  
 }
 ?>
